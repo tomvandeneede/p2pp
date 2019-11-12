b367d6206ecd26b1f2ad6f051f019f2d043ab2be
Clifford Roche <clifford.roche@gmail.com>
2019-11-12 00:55:03 -0500

    Easier compare with source file

--- tests/test_gcode.py
+++ tests/test_gcode.py
@@ -34,6 +34,11 @@ class TestGCodeCommand(unittest2.TestCase):
         self.assertFalse(g.has_parameter("Y"))
         self.assertFalse(g.has_parameter("Z"))
 
+    def test_g1_output_format_is_correct(self):
+        g = GCodeCommand("G1 F10 E10 Z10 Y10 X10")
+        o = str(g)
+        self.assertEqual(o, "G1 X10.000 Y10.000 Z10.000 E10.0000 F10")
+
     def test_parameter_is_updated(self):
         g = GCodeCommand("G1 X1")
         g.update_parameter("X", 10)
